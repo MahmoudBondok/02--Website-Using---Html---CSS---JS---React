5b303847ecf2b4f28ad8df7b9306ba73c0b450e5
Mahmoud Bondok <111879661+MahmoudBondok@users.noreply.github.com>
2022-08-27 13:39:30 +0200

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,69 @@
+import { faAppleWhole, faCoffee, faRegistered, faDesktop, faWarehouse, faOilCan } from '@fortawesome/free-solid-svg-icons'
+
+const Data = {
+    Services: [
+        {
+            icon: faAppleWhole,
+            title: 'Digital Design',
+            text: 'Lorem ipsum dolor sit amet consectetur, Dolore adipisicing elit. Dolore molestias',
+        },
+        {
+            icon: faCoffee,
+            title: 'Digital Design',
+            text: 'Lorem ipsum dolor sit amet consectetur, Dolore adipisicing elit. Dolore molestias',
+        },
+        {
+            icon: faRegistered,
+            title: 'Digital Design',
+            text: 'Lorem ipsum dolor sit amet consectetur, Dolore adipisicing elit. Dolore molestias',
+        },
+        {
+            icon: faDesktop,
+            title: 'Digital Design',
+            text: 'Lorem ipsum dolor sit amet consectetur, Dolore adipisicing elit. Dolore molestias',
+        },
+        {
+            icon: faWarehouse,
+            title: 'Digital Design',
+            text: 'Lorem ipsum dolor sit amet consectetur, Dolore adipisicing elit. Dolore molestias',
+        },
+        {
+            icon: faOilCan,
+            title: 'Digital Design',
+            text: 'Lorem ipsum dolor sit amet consectetur, Dolore adipisicing elit. Dolore molestias',
+        }
+    ],
+    pricing: [
+        {
+            title: 'ECONOMY',
+            Price: '$9.99',
+            time: 'BILLING PER MO',
+            Bandwidht: '1GB',
+            Onlinespace: '50MB',
+            Support: 'No',
+            Domain: '1',
+            HeddinFees: 'No'
+        },
+        {
+            title: 'ECONOMY',
+            Price: '$14.99',
+            time: 'BILLING PER MO',
+            Bandwidht: '5GB',
+            Onlinespace: '100MB',
+            Support: 'Yes',
+            Domain: '1',
+            HeddinFees: 'No'
+        },
+        {
+            title: 'ECONOMY',
+            Price: '$19.99',
+            time: 'BILLING PER MO',
+            Bandwidht: '10GB',
+            Onlinespace: '200MB',
+            Support: 'Yes',
+            Domain: '1',
+            HeddinFees: 'Yws',
+        }
+    ]
+}
+export default Data;
